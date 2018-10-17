162a0727dcfa6fa110608723805db14588f42aeb
Ellyn Butler <ebutler@chead.uphs.upenn.edu>
2018-10-17 17:00:16 -0400

    Oct 15 Update

--- process/hippo_seg.sh
+++ process/hippo_seg.sh
@@ -7,43 +7,40 @@
 
 T1_subjects=/home/ebutler/subjlists/nasa/subjlist_T1.csv
 T2_subjects=/home/ebutler/subjlists/nasa/subjlist_T2.csv
+processed_dir=/data/joy/BBL/studies/nasa_antartica/processedData/hippocampus
 
-for i in `seq 1 143`;
+# max 143
+for i in `seq 1 10`; do
 	# ---identify the T1 image---
-	T1_image=`sed "${i}q;d" $T1_subjects` ;
+	T1_image=`sed "${i}q;d" $T1_subjects`
 
 	# ---identify the T2 image---
-	T2_image=`sed "${i}q;d" $T2_subjects` ;
+	T2_image=`sed "${i}q;d" $T2_subjects`
 
 	# ---make the output directory---
-	winter=`echo $T1_image | cut -d '/' -f 8` ;
-	subj=`echo $T1_image | cut -d '/' -f 9` ; 
-	time=`echo $T1_image | cut -d '/' -f 10` ;
-	processed_dir=/data/joy/BBL/studies/nasa_antartica/processedData/hippocampus
-	if [ ! -e $processed_dir/$winter/ ] ; 
+	winter=`echo $T1_image | cut -d '/' -f 8`
+	subj=`echo $T1_image | cut -d '/' -f 9`
+	time=`echo $T1_image | cut -d '/' -f 10`
+
+	if [ ! -e $processed_dir/$winter/ ] ;
 		mkdir $processed_dir/$winter/ ;
-	fi
 	if [ ! -e $processed_dir/$winter/$subj/ ] ; 
 		mkdir $processed_dir/$winter/$subj/ ;
-	fi
 	if [ ! -e $processed_dir/$winter/$subj/$time/ ] ; 
 		mkdir $processed_dir/$winter/$subj/$time/ ;
 	fi	
 
 	output_dir=$processed_dir/$winter/$subj/$time/ ; 
-	# ---get images off of CHEAD onto workspace of itk snap on local machine---
-
-	
 
 	# ---add images to workspace---
-	itksnap-wt -layers-set-main $T2_image -tags-add T2_MRI \ 
-		-laters-add-anat $T1_image -tags-add T1-MRI \
+	/data/joy/BBL/applications/itksnap-3.8.0-beta/bin/itksnap-wt -layers-set-main $T2_image -tags-add T2_MRI \ 
+		-layers-add-anat $T1_image -tags-add T1-MRI \
 		-layers-list -o $output_dir/hipp_seg
 
 	# ---send workspace to cloud--- #ERB: Adapt for local machine
 	#ERB: Find services manually... get number using "itksnap-wt -dss-services-list"... use Princeton Young Adult Atlas... figure out if the service code is stable over time
-	service_code=`itksnap-wt -dss-services-list | `
-	/data/picsl/pauly/bin/itksnap-wt -i $output_dir/hipp_seg -dss-tickets-create $service_code
+	service_code=`/data/joy/BBL/applications/itksnap-3.8.0-beta/bin/itksnap-wt -dss-services-list | `
+	/data/joy/BBL/applications/itksnap-3.8.0-beta/bin/itksnap-wt -i $output_dir/hipp_seg -dss-tickets-create $service_code
 	
 done
 
