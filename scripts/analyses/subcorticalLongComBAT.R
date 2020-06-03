14cc0a0e1c81c234205b89dbdc9f9c1c081a07e0
Ellyn Butler <ellynrbutler@gmail.com>
2020-06-03 13:28:25 -0400

    Plot combatted data

--- scripts/analyses/subcorticalLongComBAT.R
+++ scripts/analyses/subcorticalLongComBAT.R
@@ -2,15 +2,14 @@
 ### a stay in Antarctica, using longitudinal combat to address site effects
 ###
 ### Ellyn Butler
-### May 28, 2020 - June 1, 2020
+### May 28, 2020 - June 3, 2020
 
 library('miceadds')
 library('ggplot2')
-library('gridExtra')
 library('ggpubr')
 library('dplyr')
 library('lme4')
-library('data.table')
+library('pbkrtest')
 source.all('~/Documents/longCombat/R')
 
 
@@ -25,10 +24,11 @@ demo_df <- demo_df[, c("subject_1", "Time", "PatientSex", "PatientAgeYears")]
 names(demo_df) <- c("subject_1", "Time", "sex", "age")
 
 # Subset volume
+subcortical <- c("vol_miccai_ave_Accumbens_Area", "vol_miccai_ave_Amygdala",
+  "vol_miccai_ave_Caudate", "vol_miccai_ave_Hippocampus", "vol_miccai_ave_Pallidum",
+  "vol_miccai_ave_Putamen", "vol_miccai_ave_Thalamus_Proper")
 vol_df <- vol_df[, c("subject_1", "Time", "scanner", "group",
- "vol_miccai_ave_Accumbens_Area", "vol_miccai_ave_Amygdala", "vol_miccai_ave_Caudate",
- "vol_miccai_ave_Hippocampus", "vol_miccai_ave_Pallidum", "vol_miccai_ave_Putamen",
- "vol_miccai_ave_Thalamus_Proper")]
+  grep("_ave_", names(vol_df), value=TRUE))]
 
 # Subset params
 params_df$combo <- paste(params_df$scanner, params_df$dx, params_df$dy, params_df$dz,
@@ -57,44 +57,52 @@ row.names(final_df) <- 1:nrow(final_df)
 final_df$Time <- recode(final_df$Time, "t1"="t0", "t2"="t0", "t3"="t0")
 
 
-################################### Model ###################################
+################################### Analysis ###################################
 
-long_df <- reshape2::melt(final_df, c("subject_1", "Time", "group", "comboGroup",
-  "age", "sex"), grep("vol_", names(final_df), value=TRUE))
-for (i in 1:nrow(long_df)) {
-  # New time variables
-  if (long_df[i, "Time"] == "t0") {
-    long_df[i, "Time2"] <- 0
-    long_df[i, "Time3"] <- 0
-  } else if (long_df[i, "Time"] == "t12") {
-    long_df[i, "Time2"] <- 1
-    long_df[i, "Time3"] <- 0
-  } else if (long_df[i, "Time"] == "t18") {
-    long_df[i, "Time2"] <- 0
-    long_df[i, "Time3"] <- 1
-  }
-}
-long_df$group <- factor(long_df$group)
 
-mod1 <- longCombat(idvar="subject_1", batchvar="comboGroup",
+mod1 <- longCombat(idvar="subject_1", batchvar="scanner",
  features=grep("vol_", names(final_df), value=TRUE),
  formula="I(group)*I(Time)", ranef="(1|subject_1)", data=final_df)
 
+data_combat <- mod1$data_combat
+data_combat <- data_combat[, subcortical]
+names(data_combat) <- paste0("combat_", names(data_combat))
+all_data <- cbind(final_df, data_combat)
+all_data <- all_data[, c("subject_1", "Time", "scanner", "group", "sex", "age",
+  grep("combat_", names(all_data), value=TRUE))]
 
+#### Plot to see if site effects gone
+for (region in grep("ol", names(all_data), value=TRUE)) {
+  all_data[,paste0("perbase_", region)] <- NA
+  for (sub in unique(all_data$subject_1)) {
+    if (all_data[all_data$subject_1 == sub, "group"] %in% c("Crew", "Control")) {
+      if ("t0" %in% all_data[all_data$subject_1 == sub, "Time"]) {
+        baseval <- all_data[all_data$subject_1 == sub & all_data$Time == "t0", region]
+        timepoints <- unique(all_data[all_data$subject_1 == sub, "Time"])
+        for (tp in timepoints) {
+          all_data[all_data$subject_1 == sub & all_data$Time == tp, paste0("perbase_", region)] <- all_data[all_data$subject_1 == sub & all_data$Time == tp, region]/baseval
+        }
+      }
+    } else {
+      baseval <- all_data[all_data$subject_1 == sub & all_data$scanner == "CGN", region]
+      scanners <- c("CGN", "HOB", "CHR")
+      for (scan in scanners) {
+        all_data[all_data$subject_1 == sub & all_data$scanner == scan, paste0("perbase_", region)] <- all_data[all_data$subject_1 == sub & all_data$scanner == scan, region]/baseval
+      }
+    }
+  }
+}
 
+#### Model
+all_data$Time2 <- with(all_data, ifelse(Time == "t12", 1,
+  ifelse(Time == "t0" | Time == "t18", 0, NA)))
+all_data$Time3 <- with(all_data, ifelse(Time == "t18", 1,
+  ifelse(Time == "t0" | Time == "t12", 0, NA)))
 
-
-
-
-
- # New group variables
- if (long_df[i, "group"] == "Crew") {
-   long_df[i, "Crew"] <- 1
-   long_df[i, "Control"] <- 0
- } else if (long_df[i, "group"] == "Control") {
-   long_df[i, "Crew"] <- 0
-   long_df[i, "Control"] <- 1
- } else if (long_df[i, "group"] == "Phantom") {
-   long_df[i, "Crew"] <- 0
-   long_df[i, "Control"] <- 0
- }
+# Linear Mixed Effects Models (maybe add 3 versus 1) Add indicator for Crew
+small_mod <- lmer(combat_vol_miccai_ave_Accumbens_Area ~ (1|subject_1) + Crew, data=all_data)
+med_mod <- lmer(combat_vol_miccai_ave_Accumbens_Area ~ (1|subject_1) + Crew + Crew:Time2, data=all_data)
+big_mod <- lmer(combat_vol_miccai_ave_Accumbens_Area ~ (1|subject_1) + Crew + Crew:Time2 + Crew:Time3, data=all_data)
+other_mod <- lmer(combat_vol_miccai_ave_Accumbens_Area ~ (1|subject_1) + Crew + Crew:Time3, data=all_data)
+KRmodcomp(med_mod, small_mod)
+KRmodcomp(big_mod, med_mod)
