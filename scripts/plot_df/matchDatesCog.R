f5d6979850109f560f8e53058d1b28e3a9778a00
Ellyn Butler <ellynrbutler@gmail.com>
2021-04-28 14:13:07 -0400

    Calculate mean accuracy rt and efficiency

--- scripts/plot_df/matchDatesCog.R
+++ scripts/plot_df/matchDatesCog.R
@@ -33,17 +33,24 @@ findClosestDate <- function(i) {
     !is.na(cog_df$CognitionDate), 'Battery']
 }
 
+calcEfficiency <- function(test) {
+  rowMeans(tmp_df[, c(paste0(test, '_ACC'), paste0(test, '_RT'))])
+}
+
 ################################################################################
 
 
 demo_df$Battery <- sapply(1:nrow(demo_df), findClosestDate)
 
-# Merge dataframes
-acc_vars <- c('MP_Accuracy', 'PVT_Accuracy', 'VOLT_pCorr', 'NBCK_pCorr',
-  'AM_pCorr', 'LOT_pCorr', 'ERT_pCorr', 'MRT_pCorr', 'DSST_pCorr')
-rt_vars <- c('MP_AvRT', 'PVT_MeanRT', 'VOLT_AvRT', 'NBCK_AvRT', 'AM_AvRT',
-  'LOT_AvRT', 'ERT_AvRT', 'MRT_AvRT', 'DSST_AvRT')
+# Rename columns
+names(cog_df) <- gsub('pCorr', 'ACC', names(cog_df))
+names(cog_df) <- gsub('AvRT', 'RT', names(cog_df))
 
+tests <- c('MP', 'PVT', 'VOLT', 'NBCK', 'AM', 'LOT', 'ERT', 'MRT', 'DSST')
+acc_vars <- paste0(tests, '_ACC')
+rt_vars <- paste0(tests, '_RT')
+
+# Merge dataframes
 final_df <- merge(demo_df[, c('subject', 'Time', 'Battery')],
   cog_df[, c('subject', 'Battery', 'SU_SleepDur', 'SU_Lonely', acc_vars, rt_vars)],
   by=c('subject', 'Battery'))
@@ -53,6 +60,8 @@ final_df[, acc_vars] <- lapply(final_df[, acc_vars], scale)
 final_df[, rt_vars] <- lapply(final_df[, rt_vars], scale)
 
 # Calculate efficiency
+final_df[, paste0('neg_', rt_vars)] <- lapply(final_df[, rt_vars], '*', -1)
+tmp_df[, paste0(tests_acc, '_EFF')] <- sapply(tests_acc, calcEfficiency)
 
 # Calculate accuracy, RT and efficiency
 final_df$Accuracy <- rowMeans(final_df[, acc_vars])
